3af42b407b95a7087e627280106ecf6e34bf38b6
PocketBrain <tim.kirp@mai.ru>
2023-09-17 13:31:09 +0300

    Models chang commit

--- models.py
+++ models.py
@@ -5,15 +5,27 @@ from ultralytics import YOLO
 class YOLOMODEL:
     def __init__(self, weights_path: str,  device: str = 'cpu'):
         model = YOLO(weights_path)
-
-    def yolo2bbox(self, bboxes) -> Tuple:
-        xmin, ymin = bboxes[0] - bboxes[2] / 2, bboxes[1] - bboxes[3] / 2
-        xmax, ymax = bboxes[0] + bboxes[2] / 2, bboxes[1] + bboxes[3] / 2
-        return xmin, ymin, xmax, ymax
-
-
-
     def predict(self, image: np.array) -> Tuple:
         results = self.model.predict(image)
+        output = []
+        for r in results:
+            boxes = r.boxes.xywh.numpy()
+            cls = r.boxes.cls.numpy()
+            scores = r.boxes.conf.numpy()
+            label_dict = r.names
+            for i in range(boxes.shape[0]):
+                x, y, w, h = boxes[i]
+                xmin, ymin = x - w / 2, y - h / 2
+                xmax, ymax = x + w / 2, y + h / 2
+                label = label_dict[cls[i]]
+                score = scores[i]
+                if label == 'window':
+                    label = 1
+                elif label == 'empty':
+                    label = 2
+                elif label == 'filled':
+                    label = 3
+                output_format = f"{label} {score} {xmin} {ymin} {xmax} {ymax}"
+                output.append(output_format)
+        return output
 
-        return selected
